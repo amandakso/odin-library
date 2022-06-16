dab3506aabf091fe4834fedbaab4e23d139245a4
Amanda So <soamanda95@gmail.com>
2022-06-16 12:08:29 -0700

    add example books to myLibrary

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
-function Book(title, author, year, pages, haveRead) {
+class Book {
+    constructor(title, author, year, pages, haveRead) {
     this.title = title;
     this.author = author;
     this.year = year;
@@ -8,9 +9,19 @@ function Book(title, author, year, pages, haveRead) {
     this.haveRead = haveRead;
     this.info = function() {
         return title + ' by ' + author + ' (' + year + ')\n' + pages + ' pages'}
+    }
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
-    console.log(myLibrary);
 }
+
+const book1 = new Book('Dune', 'Frank Herbert', 1984, 687, "haven't read")
+const book2 = new Book('The Amulet of Samarkand', 'Jonathan Stroud', 2003, 462, "haven't read")
+const book3 = new Book('Pride & Prejudice', 'Jane Austen', 1813, 386, 'have read')
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+console.log(myLibrary);
