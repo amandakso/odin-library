de6e5c0053ab5973d0726c2065392b9d80c876a0
Amanda So <soamanda95@gmail.com>
2022-06-16 15:00:28 -0700

    add popup form for new books

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 const list = document.getElementById("cards");
 
+//class for books
 class Book {
     constructor(title, author, year, pages, haveRead) {
     this.title = title;
@@ -13,6 +14,7 @@ class Book {
     }
 }
 
+// add book to library
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
@@ -25,9 +27,26 @@ addBookToLibrary(book1);
 addBookToLibrary(book2);
 addBookToLibrary(book3);
 
+// display books in list
 let books = myLibrary;
 books.forEach(book => {
     let item = document.createElement("li");
     item.innerText = book.info();
     list.prepend(item);
 })
+
+
+//Button event listeners for adding books
+
+//Show popup form
+const add = document.getElementById("add");
+add.addEventListener("click", () => {
+    document.getElementById("newForm").style.display = "block";
+});
+
+//Hide popup form
+const closed = document.getElementById("close");
+closed.addEventListener("click", () => {
+    document.getElementById("newForm").style.display = "none";
+})
+
